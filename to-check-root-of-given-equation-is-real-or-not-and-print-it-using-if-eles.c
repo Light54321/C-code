2aa50408ed13a7ddbdcde9bf2208c21592b6684e
Aditya singh <rajsinght932@gmail.com>
2024-10-17 07:21:54 +0530

    5

--- to-check-root-of-given-equation-is-real-or-not-and-print-it-using-if-eles.c
+++ to-check-root-of-given-equation-is-real-or-not-and-print-it-using-if-eles.c
@@ -2,18 +2,22 @@
 #include<math.h>
 void main()
 {
-int a,b,c,d;
-printf("Enter the cofficient of (x^2),(x)and constant\n");
+int a,b,c,D,r1,r2,i=pow(-1,0.5);
+printf("Enter the cofficient of (x^2),(x)and constant:\n");
 scanf("%d%d%d",&a,&b,&c);
-d=((b*b)-(4*a*c));
- if (d>=0)
+D=((b*b)-(4*a*c));
+ if (D>=0)
     {
-        printf("equation have real root");
+        printf("equation have real root\n");
     }
 
     else
     {
-        printf("equation have no real root");
+        printf("equation have no real root\n");
     }
+ r1=(-b+pow(D,0.5))/4*a*c;
+ r2=(-b-pow(D,0.5))/4*a*c;
+printf("the root of equation are r1=%d,r2=%d",r1,r2);
+
 
 }
---
+++ to-print-greatest-of-three-number-using-nesterd-if-else.c
@@ -0,0 +1,31 @@
+ #include<stdio.h>
+ void main( )
+{
+ float a,b,c;
+printf("Enter the  number a,b,c:\n");
+ scanf("%f%f%f",&a,&b,&c);
+if (a>b)
+{
+if (a>c)
+{
+printf("A is greatest");
+}
+else
+{
+    printf("C is greatest");
+}
+}
+if(b>c)
+{
+ printf("B is greatest");
+}
+else
+{
+    printf("cis greatest");
+}
+}
+
+
+
+
+
