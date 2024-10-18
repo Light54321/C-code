ec7ebea9a491dfe29d2233307d7d32a3e5278705
Aditya singh <rajsinght932@gmail.com>
2024-10-18 15:14:02 +0530

    5

--- for-calculating-the-value-of-celling-and-floor.c
+++ for-calculating-the-value-of-celling-and-floor.c
@@ -2,13 +2,14 @@
 #include <math.h>
 void main()
 {
-    float a,b,c;
+    double a,b,c;
     printf("Enter the number:\n");
-    scanf("%f",&c);
+    scanf("%lf",&c);
     a=ceil(c);
     b=floor(c);
     //  celing is greater integer funtion  
-    printf(" celing of number:%f\n",a);
-    //celing is smaller integer funtion  
-     printf(" floor of number:%f\n",b);
+    printf(" celing of number:%.2lf\n",a);
+    //celing is smaller integer funtion
+    // .2lf% to only 2 digit print  after decimal   
+     printf(" floor of number:%.2lf\n",b);
 }
