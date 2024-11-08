27d919e97731f0bcdcca352f37df65922fa4b787
Aditya singh <rajsinght932@gmail.com>
2024-11-08 15:59:30 +0530

    change

--- to-perform-sum-of-even-and-odd-number.c
+++ to-perform-sum-of-even-and-odd-number.c
@@ -2,34 +2,20 @@
 #include<conio.h>
 int main()
 {
-int i,n ;
+int i,n,sum=0 ;
 printf("please enter the number=");
 scanf("%d",n);
 for ( i=0; i<=1000; i+1)
 {
     if (n%2==0)
     {
-        /* code */
+    sum=sum+n;
     }
-    
+    else
+    {
+       sum=sum+n;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+printf("sum of the number=%d",sum);
 
 }
