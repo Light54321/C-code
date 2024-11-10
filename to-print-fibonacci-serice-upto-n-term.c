bd0cde59876bb8af51d696a019a6992f1e4a5494
Aditya singh <rajsinght932@gmail.com>
2024-11-10 13:41:43 +0530

    it was easy as campare to what i think.

---
+++ to-print-fibonacci-serice-upto-n-term.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{int i,n_1=0,n_2=1,term;
+printf("0");
+   for ( i=2;i<=10000;i+1)
+   {
+     term=(n_1)+(n_2);
+         n_1=n_2;
+        n_2=term;
+        printf("%d",term);
+   }
+   }
