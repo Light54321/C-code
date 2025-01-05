450d0b7ae37b6573b44518b96d1262ba7e7ef152
Light54321 <rajsinght932@gmail.com>
2025-01-05 18:21:52 +0530

    getcharc

---
+++ String/important_string_point/with_getcha_funtion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char str[10];
+    printf("Enter a chR :\n");
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%c", &str[i]);
+        char ch = getchar();
+    }
+    printf("The 7th chR is %c", str[6]);
+    return 0;
+}
---
+++ String/important_string_point/without_getchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char str[10];
+    printf("Enter a chR :\n");
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%c", &str[i]);
+    }
+    printf("The 7th chR is %c", str[6]);
+    return 0;
+}
